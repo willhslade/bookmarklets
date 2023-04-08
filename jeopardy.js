60227797fa96f90456fd22f5065e4d690aff8448
Will Slade <willhslade@gmail.com>
2023-04-07 20:34:46 -0400

    Update jeopardy.js

--- jeopardy.js
+++ jeopardy.js
@@ -1,1 +1,24 @@
-javascript:function url() { var date = new Date(); date.setDate(date.getDate() - 1); var y = date.getFullYear();  var m = date.getMonth() + 1; if(m < 10){m = '0' + m;} var d = date.getDate(); if(d < 10){d = '0' + d;} var date = m + '/' + d + '/' + y; return 'https://www.google.com/search?q=jeopardy full episode youtube %27 + date;} window.open(url(),"_blank");
+javascript:function url() { var date = new Date(); var backup = 1; if (date.getDay() === 0) {backup = 2;} if (date.getDay() === 1) {backup = 3;} date.setDate(date.getDate() - backup); var y = date.getFullYear();  var m = date.getMonth() + 1; if(m < 10){m = '0' + m;} var d = date.getDate(); if(d < 10){d = '0' + d;} return 'https://www.google.com/search?q=jeopardy+full+episode+youtube+%27 + m + %27/%27 + d + %27/%27 + y;} window.open(url(),"_blank");
+
+javascript: function url() {
+        var date = new Date();
+        var backup = 1;
+        if (date.getDay() === 0) {
+            backup = 2;
+        }
+        if (date.getDay() === 1) {
+            backup = 3;
+        }
+        date.setDate(date.getDate() - backup);
+        var y = date.getFullYear();
+        var m = date.getMonth() + 1;
+        if (m < 10) {
+            m = '0' + m;
+        }
+        var d = date.getDate();
+        if (d < 10) {
+            d = '0' + d;
+        }
+        return 'https://www.google.com/search?q=jeopardy+full+episode+youtube+%27 + m + %27/%27 + d + %27/%27 + y;
+} 
+window.open(url(),"_blank");
